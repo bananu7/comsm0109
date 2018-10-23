50ba6b456100f11dc49f9c033e9bc52429bb0e2e
Elliot Potts <elliotpotts@gmail.com>
2018-10-23 02:58:48 +0100

    instruction codec'ing just doesn't work

--- sim/main.cpp
+++ sim/main.cpp
@@ -33,13 +33,23 @@ class cpu {
             break;
         }
         case state::fetch: {
+            state = state::decode;
             break;
         }
         case state::decode: {
             decoded = decode(get(get(reg::pc)));
+            state = state::execute;
             break;
         }
         case state::execute:
+            std::cout << "Executing at " << get(reg::pc) << ": " << decoded.value() << "\n";
+            std::visit( [&](auto i) {
+                if constexpr(std::is_same_v<decltype(i), add>) {
+                    std::cout << i.dst << " = " << get(i.lhs) << " + " << get(i.rhs) << "\n";
+                    get(i.dst) = get(i.lhs) + get(i.rhs);
+                }
+            }, decoded.value());
+            get(reg::pc)++;
             return;
         }
     }
@@ -50,22 +60,33 @@ public:
     }
 
     // Clear the memory, Load the given binary, set pc = 1 and run. return the value in reg::g1 when pc=0
-    std::int32_t operator()(const std::vector<std::int8_t> binary) {
+    std::int32_t operator()(const std::vector<std::uint8_t> binary) {
         if(binary.size() > main_memory.size()) {
             throw std::runtime_error("Binary too large!");
         }
         main_memory.fill(0);
         register_file.fill(0);
+        get(reg::one) = 1;
         std::copy(binary.begin(), binary.end(), main_memory.begin());
-        get(reg::pc) == 1;
-        while(get(reg::pc) != 0) {
+        // start at 0x1
+        get(reg::pc) = 2;
+        state = state::fetch;
+        while(get(reg::pc) != 0 && get(reg::pc) <= static_cast<int>(binary.size() / 2)) {
             step();
         }
+        return get(reg::g1);
     }
 };
 
 int main() {
-    instruction i = add { reg::g1, reg::g2, reg::g3 };
-    std::cout << i << "\n";
+    //const instruction program = add { reg::one, reg::one, reg::g1 };
+    std::vector<std::uint8_t> progbin(4);
+    //*(reinterpret_cast<std::uint16_t*>(progbin.data()) + 1) = encode(program);
+    *(reinterpret_cast<std::uint16_t*>(progbin.data()) + 1) = 0b0000001001011000;
+    std::cout << decode(*(reinterpret_cast<std::uint16_t*>(progbin.data()) + 1)) << "\n---\n";
+
+    cpu machine;
+    int result = machine(progbin);
+    std::cout << "---\n  program says 1 + 1 = " << result << "\n";
     return 0;
 }
